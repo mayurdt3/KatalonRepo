1cf44ce5723f31e00861f48aaf1e543a6d431d4f
mayurdt3 <mayurtakalikar3@gmail.com>
2023-12-22 23:51:24 +0530

    c18

--- Test Suites/Swaglabs/Visual Swag Labs.ts
+++ Test Suites/Swaglabs/Visual Swag Labs.ts
@@ -12,45 +12,80 @@
    <rerunImmediately>true</rerunImmediately>
    <testSuiteGuid>da3f51bb-9049-4e3e-b7f1-1aa02a78c60f</testSuiteGuid>
    <testCaseLink>
-      <guid>58737298-693b-4fd1-b50b-6f5b1588be81</guid>
+      <guid>12ecfade-8011-45b8-926b-34a1f0a09f70</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
-      <testCaseId>Test Cases/SagLabs/Visual Test case/Invalid Login</testCaseId>
+      <testCaseId>Test Cases/SagLabs/Visual Test case/Add to cart</testCaseId>
       <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
    </testCaseLink>
    <testCaseLink>
-      <guid>fa508faf-2d3f-4e1f-97da-8c41cad4d405</guid>
+      <guid>35838515-7fe4-4ed2-a3c7-c2a0887a3b69</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
-      <testCaseId>Test Cases/SagLabs/Visual Test case/Login</testCaseId>
+      <testCaseId>Test Cases/SagLabs/Visual Test case/Checkout</testCaseId>
       <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
    </testCaseLink>
    <testCaseLink>
-      <guid>a263e75b-e379-4fc6-8c0d-649140580a8d</guid>
+      <guid>312261fc-9290-4845-9706-5ad0d4330533</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/SagLabs/Visual Test case/Continue Shopping</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>b8056b21-fadc-4602-a177-e03bd78a9c78</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
       <testCaseId>Test Cases/SagLabs/Visual Test case/Empty Cart</testCaseId>
       <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
    </testCaseLink>
    <testCaseLink>
-      <guid>40406309-43fa-43b5-afd6-010130d2543a</guid>
+      <guid>980297c3-c8b0-44e3-ac29-70dd9220b666</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/SagLabs/Visual Test case/Invalid Login</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>bf3ebe40-2d17-4bcd-a4e1-37fb2fe5ec21</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/SagLabs/Visual Test case/Login</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>57b8193e-56c6-48c1-9c5a-b92c0e84a500</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
       <testCaseId>Test Cases/SagLabs/Visual Test case/Logout</testCaseId>
       <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
    </testCaseLink>
    <testCaseLink>
-      <guid>e51465b2-cd8b-4150-9c37-55cdfb6f9738</guid>
+      <guid>06f884c5-c642-4a5f-b0e5-fa77814d7cad</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/SagLabs/Visual Test case/Navigaet to Login</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>13874a11-1344-45f6-bfd0-860e02f7a725</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/SagLabs/Visual Test case/Navigate to cart after add to cart</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>3c30113c-b712-4f8f-863d-9b3d9ee7177b</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
       <testCaseId>Test Cases/SagLabs/Visual Test case/Product Listing Page</testCaseId>
       <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
    </testCaseLink>
    <testCaseLink>
-      <guid>2977c6ae-32e0-46e8-8a92-5caf19ab4d7f</guid>
+      <guid>8db044dc-5e98-4389-b6d6-eedb012aafce</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
-      <testCaseId>Test Cases/SagLabs/Visual Test case/Navigaet to Login</testCaseId>
+      <testCaseId>Test Cases/SagLabs/Visual Test case/Remove product from cart</testCaseId>
       <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
    </testCaseLink>
 </TestSuiteEntity>
